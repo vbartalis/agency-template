ed94f408055918116fd01ba130149782431a2f9b
Iswar Patel <ispatel@fb.com>
2024-12-09 08:30:20 -0500

    create prices data

---
+++ src/utils/data/prices.ts
@@ -0,0 +1,20 @@
+export const prices = {
+    1: {
+        pricePerPair: 69.99,
+        originalPrice: 139.99,
+        savePercentage: 50,
+        badge: "",
+    },
+    2: {
+        pricePerPair: 62.99,
+        originalPrice: 139.99,
+        savePercentage: 55,
+        badge: "Most Popular",
+    },
+    3: {
+        pricePerPair: 55.99,
+        originalPrice: 139.99,
+        savePercentage: 60,
+        badge: "Best Deal",
+    },
+};
