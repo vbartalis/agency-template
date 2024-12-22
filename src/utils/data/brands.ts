cd3911e29261b0dbccbaeda30a037f9c06e49e84
prayashrpatel <prayash.patel0820@gmail.com>
2024-12-22 13:17:43 -0800

    fixed brand logos

--- src/utils/data/brands.ts
+++ src/utils/data/brands.ts
@@ -3,33 +3,34 @@ const brands = {
   list: [
     {
       id: 1,
-      name: "Spotify",
-      logo: "/src/assets/logos/spotify.png"
+      name: "Tik Tok",
+      logo: "/src/assets/logos/tikTokLogo.png"
     },
     {
       id: 2,
-      name: "Slack",
-      logo: "/src/assets/logos/slack.png"
+      name: "Forbes",
+      logo: "/src/assets/logos/forbesLogo.png"
+
     },
     {
       id: 3,
-      name: "Paypal",
-      logo: "/src/assets/logos/paypallogo.png"
+      name: "USA Today",
+      logo: "/src/assets/logos/usaTodayLogo.png"
     },
     {
       id: 4,
-      name: "Spotify",
-      logo: "/src/assets/logos/spotify.png"
+      name: "Wired",
+      logo: "/src/assets/logos/wiredLogo.png"
     },
     {
       id: 5,
-      name: "Slack",
-      logo: "/src/assets/logos/slack.png"
+      name: "Vogue Business",
+      logo: "/src/assets/logos/vogueBusiness.png"
     },
     {
       id: 6,
-      name: "Paypal",
-      logo: "/src/assets/logos/paypallogo.png"
+      name: "New York Times",
+      logo: "/src/assets/logos/theNewYorkTimes.png"
     }
   ]
 };
