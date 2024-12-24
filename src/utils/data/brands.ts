200c7fdebbbd73a6de42de9987b1974ac0ec6b2f
prayashrpatel <prayash.patel0820@gmail.com>
2024-12-24 11:06:56 -0800

    Channged all pngs to webps

--- src/utils/data/brands.ts
+++ src/utils/data/brands.ts
@@ -4,33 +4,33 @@ const brands = {
     {
       id: 1,
       name: "Tik Tok",
-      logo: "/src/assets/logos/tikTokLogo.png"
+      logo: "/src/assets/logos/tikTokLogo.webp"
     },
     {
       id: 2,
       name: "Forbes",
-      logo: "/src/assets/logos/forbesLogo.png"
+      logo: "/src/assets/logos/forbesLogo.webp"
 
     },
     {
       id: 3,
       name: "USA Today",
-      logo: "/src/assets/logos/usaTodayLogo.png"
+      logo: "/src/assets/logos/usaTodayLogo.webp"
     },
     {
       id: 4,
       name: "Wired",
-      logo: "/src/assets/logos/wiredLogo.png"
+      logo: "/src/assets/logos/wiredLogo.webp"
     },
     {
       id: 5,
       name: "Vogue Business",
-      logo: "/src/assets/logos/vogueBusiness.png"
+      logo: "/src/assets/logos/vogueBusiness.webp"
     },
     {
       id: 6,
       name: "New York Times",
-      logo: "/src/assets/logos/theNewYorkTimes.png"
+      logo: "/src/assets/logos/theNewYorkTimes.webp"
     }
   ]
 };
