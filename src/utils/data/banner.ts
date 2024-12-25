9bc67e1caedc7ffaf6f462441194f80c215c7788
prayashrpatel <prayash.patel0820@gmail.com>
2024-12-25 14:40:12 -0800

    change banner text

--- src/utils/data/banner.ts
+++ src/utils/data/banner.ts
@@ -1,4 +1,4 @@
-const banner = ["HAPPY HOLIDAYS SALE!🎄 1 PAIR 50% OFF or 3 PAIRS 60% OFF", "FREE SHIPPING ON ORDERS"]
+const banner = ["HAPPY HOLIDAYS SALE!🎄 UPTO 60% OFF", "FREE SHIPPING ON ORDERS"]
 
 export {
     banner
