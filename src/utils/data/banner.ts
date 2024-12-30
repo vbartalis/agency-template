22f3a21ba56cfff915ae515b57e2fc1778b74435
prayashrpatel <prayash.patel0820@gmail.com>
2024-12-30 13:58:53 -0800

    fixed emojis in banner

--- src/utils/data/banner.ts
+++ src/utils/data/banner.ts
@@ -1,4 +1,4 @@
-const banner = ["🎉 HAPPY HOLIDAYS SALE!🎄 UPTO 60% OFF", "FREE SHIPPING ON ALL ORDERS", "🔒 100% ENCRYPTED & SECURE CHECKOUT"]
+const banner = ["🎉 HAPPY HOLIDAYS SALE!🎄 UP TO 60% OFF", "🚚FREE SHIPPING ON ALL ORDERS!", "🔒 100% ENCRYPTED & SECURE CHECKOUT"]
 
 export {
     banner
